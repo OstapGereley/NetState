49214317fd2eeda59194007ba880aeb3d23a4ca3
Sergey Yacovlev <sergio.yacovlev@outlook.com>
2017-11-19 19:42:37 +0000

    TArg. Naming consinstency.

--- NetState.Core.Barebones/StatementMetadatas/IStatementMetadata_8Args.cs
+++ NetState.Core.Barebones/StatementMetadatas/IStatementMetadata_8Args.cs
@@ -1,12 +1,12 @@
 ﻿namespace NetState.Core.Barebones.StatementMetadatas {
 
-    public interface IStatementMetadata<TArg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7, Arg8, R> : INotVoidStatementMetadata<R> {
+    public interface IStatementMetadata<TArg1, TArg2, TArg3, Arg4, Arg5, Arg6, Arg7, Arg8, R> : INotVoidStatementMetadata<R> {
 
         IParameterMetadata<TArg1> Argument1 { get; }
 
-        IParameterMetadata<Arg2> Argument2 { get; }
+        IParameterMetadata<TArg2> Argument2 { get; }
 
-        IParameterMetadata<Arg3> Argument3 { get; }
+        IParameterMetadata<TArg3> Argument3 { get; }
 
         IParameterMetadata<Arg4> Argument4 { get; }
 
